204812cda3f550e992b4443dcd597c83962f20fe
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-23 10:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function removeColor(){
+	const value = document.querySelector('#colorSelect');
+const selectedOption = value.options[value.selectedIndex];
+value.removeChild(selectedOption);
+}
+const btn = document.querySelector('.btn');
+btn.addEventListener('click',()=>{
+	removeColor();
+})
